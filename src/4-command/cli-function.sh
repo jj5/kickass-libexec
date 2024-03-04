85e3036d837fa5de61e7b4af28456c8302e0f689
John Elliot V <jj5@jj5.net>
2024-03-04 22:10:26 +1100

    Work, work...

--- src/4-command/cli-function.sh
+++ src/4-command/cli-function.sh
@@ -12,6 +12,16 @@ p() {
 
 }
 
+sudo() {
+
+  /usr/bin/sudo "$@"
+
+  # 2024-03-04 jj5 - set the window title back to the localhost
+
+  echo -ne "\033]2;$USER@$HOSTNAME\007" >&2
+
+}
+
 ssh() {
 
   /usr/bin/ssh "$@"
