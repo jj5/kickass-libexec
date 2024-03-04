6fbd445a7b963a3e5ca7a218cc11173f11b138fd
John Elliot V <jj5@jj5.net>
2024-03-04 21:12:05 +1100

    Work, work...

---
+++ bin/lx-maint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+main() {
+
+  lx_maint "$@";
+
+}
+
+source "$( dirname "${BASH_SOURCE[0]}" )/../inc/run.sh";
---
+++ src/2-module/maint/maint.sh
@@ -0,0 +1,132 @@
+#!/bin/bash
+
+lx_maint() {
+
+  local svn_wc_list=(
+    /home/jj5/bin
+    /home/jj5/bin/private
+  );
+
+  for path in "${svn_wc_list[@]}"; do
+
+    if [ -d "$path" ]; then
+
+      lx_run sudo -u jj5 svn up "$path";
+
+    fi;
+
+  done;
+
+  local git_wc_list=(
+    /srv/libexec
+    /srv/admin
+    /srv/pillar
+    /srv/salt
+  );
+
+  for path in "${git_wc_list[@]}"; do
+
+    if [ -d "$path" ]; then
+
+      lx_quiet pushd "$path";
+
+      lx_run sudo git pull;
+
+      lx_quiet popd;
+
+    fi;
+
+  done;
+
+  lx_run sudo apt update;
+
+  lx_run sudo apt -y dist-upgrade;
+
+  lx_run sudo apt -y autoremove;
+
+  if command -v snap; then
+
+    lx_run sudo snap refresh;
+
+  fi
+
+  if systemctl is-active --quiet zabbix-agent.service; then
+
+    lx_note "zabbix agent still running, will disable."
+
+    lx_run sudo systemctl disable zabbix-agent.service;
+
+  fi
+
+  if command -v salt-call; then
+
+    # 2022-10-06 jj5 - if you're debugging try:
+    # salt-call --state-output=mixed --state-verbose=False --log-file-level=all state.highstate
+    # the log file is /var/log/salt/minion
+
+    # 2022-11-28 jj5 - NOTE: some systems have broken salt config and are "snow flakes" until
+    # I can manually replace/upgrade them...
+    #
+    if lx_is_snowflake; then
+
+      lx_note "salt-call disabled as '$HOSTNAME' is a snow-flake.";
+
+      if systemctl is-active --quiet salt-minion.service; then
+
+        lx_note "disabling salt-minion...";
+
+        lx_run sudo systemctl disable salt-minion
+
+      fi
+
+    else
+
+      lx_run sudo salt-call --state-output=mixed --state-verbose=False state.highstate;
+
+    fi
+
+  fi;
+
+  # 2024-03-04 jj5 - THINK: put this back in?
+  #disk-usage.sh;
+
+  if command -v zpool >/dev/null; then
+
+    lx_run zpool list;
+
+  fi;
+
+}
+
+lx_is_snowflake() {
+
+  # 2024-03-04 jj5 - these are systems which we no longer manage with salt...
+
+  local snowflake=(
+    wit
+    integrity
+    platinum
+    curiosity
+    charm
+    peace
+    defiance
+    jjdev
+    studiousness
+    avdev
+    cldev
+    fbdev
+  );
+
+  local host='';
+
+  for host in "${snowflake[@]}"; do
+
+    if [ "$HOSTNAME" = "$host" ]; then return 0; fi
+
+  done;
+
+  return 1;
+
+}
+
+main "$@";
