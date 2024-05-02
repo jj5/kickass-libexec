3413859c9860cbe42cdbf51925b48e22c96ffecf
John Elliot V <jj5@jj5.net>
2024-05-02 15:56:56 +1000

    Work, work...

--- inc/bashrc.sh
+++ inc/bashrc.sh
@@ -21,8 +21,8 @@ source "$LX_DIR_SRC/4-command/cli-alias.sh";
 export LS_OPTIONS='--color=auto'
 eval "$(dircolors)"
 alias ls='ls $LS_OPTIONS'
-alias ll='ls $LS_OPTIONS -l'
-alias l='ls $LS_OPTIONS -lA'
+alias ll='ls $LS_OPTIONS -alh'
+#alias l='ls $LS_OPTIONS -lA'
 
 # Some more alias to avoid making mistakes:
 [ "$USER" == "root" ] && {
