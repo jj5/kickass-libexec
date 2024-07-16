69a0f7d4a944d85a5d4375c1e6d4ae9d0e808eaa
John Elliot V <jj5@jj5.net>
2024-07-17 02:42:01 +1000

    Work, work...

--- src/1-toolkit/toolkit.sh
+++ src/1-toolkit/toolkit.sh
@@ -771,6 +771,8 @@ lx_loop() {
   while [ true ]
   do
 
+    lx_daylight;
+
     # 2019-12-23 jj5 - under this new scheme the only thing that should be
     # able to stop the loop is the stop file, the loop will iterate even if
     # the previous iteration failed.
@@ -863,6 +865,8 @@ lx_run_job() {
 
   local description="$1"; shift;
 
+  lx_daylight;
+
   lx_newline;
 
   lx_report "${LX_WHITE}job: ${description}...${LX_END}";
@@ -915,6 +919,8 @@ lx_try_as() {
 lx_try() {
 
   local description="running $*";
+
+  lx_daylight;
 
   lx_newline;
 
@@ -1222,6 +1228,58 @@ lx_notify() {
     lx_fail "invalid mailer '$mailer'.";
 
   fi;
+
+}
+
+# 2024-07-16 jj5 - the point of this function is to delay for four hours around daylight savings time change. It is
+# integrated as part of the framework/toolkit and will be called automatically before main() is called and from time
+# to time from some key functions such as lx_run() and lx_try().
+#
+lx_daylight() {
+
+  # 2024-07-16 jj5 - NOTE: this code hasn't been well tested.
+
+  # 2024-07-16 jj5 - get the current timestamp
+  #
+  local current_timestamp=$( date +%s );
+
+  # 2024-07-16 jj5 - get the current offset in seconds from UTC
+  #
+  local current_offset=$( date +%z | awk '{print ($1 / 100) * 3600}' );
+
+  # 2024-07-16 jj5 - calculate the timestamps plus or minus two hours from now
+  #
+  local two_hours_before=$((  current_timestamp - 2 * 3600 ));
+  local two_hours_later=$((   current_timestamp + 2 * 3600 ));
+
+  # 2024-07-16 jj5 - get the offsets in seconds from UTC for plus or minus two hours from now
+  #
+  local two_hours_before_offset=$(  date -d @${two_hours_before}  +%z | awk '{print ($1 / 100) * 3600}');
+  local two_hours_later_offset=$(   date -d @${two_hours_later}   +%z | awk '{print ($1 / 100) * 3600}');
+
+  # 2024-07-16 jj5 - check if the offset changes within two hours either side of the current time
+  #
+  if [ "$current_offset" -ne "$two_hours_before_offset" ]; then
+
+    lx_newline;
+
+    lx_note "daylight savings just changed, will sleep for 2 hours.";
+
+    sleep 2h;
+    sleep 37;
+
+  elif [ "$current_offset" -ne "$two_hours_later_offset" ]; then
+
+    lx_newline;
+
+    lx_note "daylight savings is about to change, will sleep for 4 hours.";
+
+    sleep 4h;
+    sleep 37;
+
+  fi
+
+  return 0;
 
 }
 
