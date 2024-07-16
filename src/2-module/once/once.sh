f69991a964317ce30b7c6a096fa29512596eaaf4
John Elliot V <jj5@jj5.net>
2024-07-17 02:20:24 +1000

    Work, work...

--- src/2-module/once/once.sh
+++ src/2-module/once/once.sh
@@ -12,13 +12,10 @@ lx_once_per_year() {
   local current_time="$( date )";
   local timestamp="$( date -d "$current_time" +"%Y-%m-%d-%H%M%S" )";
   local process_dir="$LX_STATE_DIR/$process_slug";
-
-  [ -d "$process_dir" ] || lx_run mkdir -p "$process_dir";
-
   local process_file="$process_dir/year-$( date -d "$current_time" +"%Y" ).log"
   local process_fail="$process_dir/year-$( date -d "$current_time" +"%Y" )-fail-$timestamp.log"
 
-  lx_run lx_once "$process_name" "$process_slug" "$process_file" "$process_fail" "$@";
+  lx_run lx_once "$process_name" "$process_dir" "$process_file" "$process_fail" "$@";
 
 }
 
@@ -34,13 +31,10 @@ lx_once_per_month() {
   local current_time="$( date )";
   local timestamp="$( date -d "$current_time" +"%Y-%m-%d-%H%M%S" )";
   local process_dir="$LX_STATE_DIR/$process_slug/$( date -d "$current_time" +"%Y" )";
-
-  [ -d "$process_dir" ] || lx_run mkdir -p "$process_dir";
-
   local process_file="$process_dir/month-$( date -d "$current_time" +"%m" ).log"
   local process_fail="$process_dir/month-$( date -d "$current_time" +"%m" )-fail-$timestamp.log"
 
-  lx_run lx_once "$process_name" "$process_slug" "$process_file" "$process_fail" "$@";
+  lx_run lx_once "$process_name" "$process_dir" "$process_file" "$process_fail" "$@";
 
 }
 
@@ -56,15 +50,11 @@ lx_once_per_week() {
   local current_time="$( date )";
   local timestamp="$( date -d "$current_time" +"%Y-%m-%d-%H%M%S" )";
   local process_dir="$LX_STATE_DIR/$process_slug/$( date -d "$current_time" +"%Y/%m" )";
-
-  [ -d "$process_dir" ] || lx_run mkdir -p "$process_dir";
-
   local current_week="$( lx_once_get_week "$current_time" )";
-
   local process_file="$process_dir/week-$current_week.log"
   local process_fail="$process_dir/week-$current_week-fail-$timestamp.log"
 
-  lx_run lx_once "$process_name" "$process_slug" "$process_file" "$process_fail" "$@";
+  lx_run lx_once "$process_name" "$process_dir" "$process_file" "$process_fail" "$@";
 
 }
 
@@ -80,13 +70,10 @@ lx_once_per_day() {
   local current_time="$( date )";
   local timestamp="$( date -d "$current_time" +"%Y-%m-%d-%H%M%S" )";
   local process_dir="$LX_STATE_DIR/$process_slug/$( date -d "$current_time" +"%Y/%m" )";
-
-  [ -d "$process_dir" ] || lx_run mkdir -p "$process_dir";
-
   local process_file="$process_dir/day-$( date -d "$current_time" +"%d" ).log"
   local process_fail="$process_dir/day-$( date -d "$current_time" +"%d" )-fail-$timestamp.log"
 
-  lx_run lx_once "$process_name" "$process_slug" "$process_file" "$process_fail" "$@";
+  lx_run lx_once "$process_name" "$process_dir" "$process_file" "$process_fail" "$@";
 
 }
 
@@ -102,44 +89,25 @@ lx_once_per_hour() {
   local current_time="$( date )";
   local timestamp="$( date -d "$current_time" +"%Y-%m-%d-%H%M%S" )";
   local process_dir="$LX_STATE_DIR/$process_slug/$( date -d "$current_time" +"%Y/%m/%d" )";
-
-  [ -d "$process_dir" ] || lx_run mkdir -p "$process_dir";
-
   local process_file="$process_dir/hour-$( date -d "$current_time" +"%H" ).log"
   local process_fail="$process_dir/hour-$( date -d "$current_time" +"%H" )-fail-$timestamp.log"
 
-  lx_run lx_once "$process_name" "$process_slug" "$process_file" "$process_fail" "$@";
-
-}
-
-lx_once_validate_env() {
-
-  [ -d "$LX_STATE_DIR" ] || {
-
-    local container="$( dirname "$LX_STATE_DIR" )";
-
-    [ -d "$container" ] || {
-
-      lx_fail "$LX_EXIT_FILE_MISSING" "state directory '$container' missing.";
-
-    };
-
-    lx_run mkdir "$LX_STATE_DIR";
-
-  }
-
-  return 0;
+  lx_run lx_once "$process_name" "$process_dir" "$process_file" "$process_fail" "$@";
 
 }
 
 lx_once() {
 
   local process_name="$1";
-  local process_slug="$2";
+  local process_dir="$2";
   local process_file="$3";
   local process_fail="$4";
 
   shift; shift; shift; shift;
+
+  lx_once_validate_env;
+
+  [ -d "$process_dir" ] || lx_run mkdir -p "$process_dir";
 
   [ -e "$process_file" ] && {
 
@@ -178,6 +146,26 @@ lx_once() {
   [ $error_level != '0' ] && { mv "$process_file" "$process_fail"; }
 
   return "$error_level";
+
+}
+
+lx_once_validate_env() {
+
+  [ -d "$LX_STATE_DIR" ] || {
+
+    local container="$( dirname "$LX_STATE_DIR" )";
+
+    [ -d "$container" ] || {
+
+      lx_fail "$LX_EXIT_FILE_MISSING" "state directory '$container' missing.";
+
+    };
+
+    lx_run mkdir "$LX_STATE_DIR";
+
+  }
+
+  return 0;
 
 }
 
