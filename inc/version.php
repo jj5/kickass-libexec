44fa296d2925d3d82c6913cbb9900f44fa230fca
John Elliot V <jj5@jj5.net>
2024-08-10 08:52:38 +1000

    Work, work...

--- inc/version.php
+++ inc/version.php
@@ -6,16 +6,16 @@
 define( 'LX_NAME', 'Kickass Libexec' );
 define( 'LX_CODE', 'lx' );
 
-//define( 'LX_VERSION', '0.1.281' );
+//define( 'LX_VERSION', '0.1.283' );
 define( 'LX_VERSION_MAJOR', 0 );
 define( 'LX_VERSION_MINOR', 1 );
-define( 'LX_VERSION_PATCH', 281 );
+define( 'LX_VERSION_PATCH', 283 );
 
 define(
   'LX_SVN_DATE',
-  '$Date: 2024-08-09 22:36:22 +0000 (Fri, 09 Aug 2024) $'
+  '$Date: 2024-08-09 22:52:38 +0000 (Fri, 09 Aug 2024) $'
 );
-define( 'LX_SVN_REVISION', '$Revision: 98212722 $' );
+define( 'LX_SVN_REVISION', '$Revision: 33658289 $' );
 define( 'LX_SVN_AUTHOR', '$Author: jj5 $' );
 
-define( 'LX_GIT_DATE', '2024-08-09 22:36:22 +0000 (Fri, 09 Aug 2024)' );
+define( 'LX_GIT_DATE', '2024-08-09 22:52:38 +0000 (Fri, 09 Aug 2024)' );
--- inc/version.sh
+++ inc/version.sh
@@ -5,13 +5,13 @@
 export LX_NAME=''Kickass Libexec'';
 export LX_CODE=''lx'';
 
-export LX_VERSION='0.1.281';
+export LX_VERSION='0.1.283';
 export LX_VERSION_MAJOR='0';
 export LX_VERSION_MINOR='1';
-export LX_VERSION_PATCH='281';
+export LX_VERSION_PATCH='283';
 
-export LX_SVN_DATE='Date: 2024-08-09 22:36:22 +0000 (Fri, 09 Aug 2024)';
-export LX_SVN_REVISION='Revision: 98212722';
+export LX_SVN_DATE='Date: 2024-08-09 22:52:38 +0000 (Fri, 09 Aug 2024)';
+export LX_SVN_REVISION='Revision: 33658289';
 export LX_SVN_AUTHOR='Author: jj5';
 
-export LX_GIT_DATE='2024-08-09 22:36:22 +0000 (Fri, 09 Aug 2024)';
+export LX_GIT_DATE='2024-08-09 22:52:38 +0000 (Fri, 09 Aug 2024)';
