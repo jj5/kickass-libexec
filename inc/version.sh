87c22b0939c956f8f12ca70776a0ea1c49d40cc8
John Elliot V <jj5@jj5.net>
2024-08-11 12:20:29 +1000

    Work, work...

--- inc/version.sh
+++ inc/version.sh
@@ -5,13 +5,13 @@
 export LX_NAME=''Kickass Libexec'';
 export LX_CODE=''lx'';
 
-export LX_VERSION='0.1.303';
+export LX_VERSION='0.1.305';
 export LX_VERSION_MAJOR='0';
 export LX_VERSION_MINOR='1';
-export LX_VERSION_PATCH='303';
+export LX_VERSION_PATCH='305';
 
-export LX_SVN_DATE='Date: 2024-08-11 12:10:51 +1000 (Sun, 11 Aug 2024)';
-export LX_SVN_REVISION='Revision: 199914293';
+export LX_SVN_DATE='Date: 2024-08-11 12:20:28 +1000 (Sun, 11 Aug 2024)';
+export LX_SVN_REVISION='Revision: 149595803';
 export LX_SVN_AUTHOR='Author: jj5';
 
-export LX_GIT_DATE='2024-08-11 12:10:51 +1000 (Sun, 11 Aug 2024)';
+export LX_GIT_DATE='2024-08-11 12:20:28 +1000 (Sun, 11 Aug 2024)';
