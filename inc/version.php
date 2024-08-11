e7498b67250d61e36c47c6e24b487c0f1f8b0247
John Elliot V <jj5@jj5.net>
2024-08-11 15:40:43 +1000

    Work, work...

--- inc/version.php
+++ inc/version.php
@@ -6,16 +6,16 @@
 define( 'LX_NAME', 'Kickass Libexec' );
 define( 'LX_CODE', 'lx' );
 
-//define( 'LX_VERSION', '0.1.335' );
+//define( 'LX_VERSION', '0.1.337' );
 define( 'LX_VERSION_MAJOR', 0 );
 define( 'LX_VERSION_MINOR', 1 );
-define( 'LX_VERSION_PATCH', 335 );
+define( 'LX_VERSION_PATCH', 337 );
 
 define(
   'LX_SVN_DATE',
-  '$Date: 2024-08-11 04:33:20 +0000 (Sun, 11 Aug 2024) $'
+  '$Date: 2024-08-11 05:40:43 +0000 (Sun, 11 Aug 2024) $'
 );
-define( 'LX_SVN_REVISION', '$Revision: 60244735 $' );
+define( 'LX_SVN_REVISION', '$Revision: 145782652 $' );
 define( 'LX_SVN_AUTHOR', '$Author: jj5 $' );
 
-define( 'LX_GIT_DATE', '2024-08-11 04:33:20 +0000 (Sun, 11 Aug 2024)' );
+define( 'LX_GIT_DATE', '2024-08-11 05:40:43 +0000 (Sun, 11 Aug 2024)' );
