f07484898286e46758ed4823a1642aeb88a1419e
John Elliot V <jj5@jj5.net>
2026-07-17 22:56:28 +1000

    Work, work...

--- inc/version.sh
+++ inc/version.sh
@@ -5,13 +5,13 @@
 export LX_NAME='lx-0.1';
 export LX_CODE='lx';
 
-export LX_VERSION='0.1.453';
+export LX_VERSION='0.1.455';
 export LX_VERSION_MAJOR='0';
 export LX_VERSION_MINOR='1';
-export LX_VERSION_PATCH='453';
+export LX_VERSION_PATCH='455';
 
-export LX_SVN_DATE='Date: 2026-07-17 11:20:52 +0000 (Fri, 17 Jul 2026)';
-export LX_SVN_REVISION='Revision: 181845659';
+export LX_SVN_DATE='Date: 2026-07-17 12:56:28 +0000 (Fri, 17 Jul 2026)';
+export LX_SVN_REVISION='Revision: 180892063';
 export LX_SVN_AUTHOR='Author: jj5';
 
-export LX_GIT_DATE='2026-07-17 11:20:52 +0000 (Fri, 17 Jul 2026)';
+export LX_GIT_DATE='2026-07-17 12:56:28 +0000 (Fri, 17 Jul 2026)';
